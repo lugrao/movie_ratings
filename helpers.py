76465dd0b70911c311dd3045c6b2f54394676b6d
lugrao <elugrao@gmail.com>
2021-04-15 18:40:19 -0300

    update api key variable in get_person()

---
+++ helpers.py
@@ -0,0 +1,247 @@
+import os
+import math
+import requests
+import tmdbsimple as tmdb
+from bs4 import BeautifulSoup
+
+
+def get_movie(title='', year='', tmdb_id='', imdb_id=''):
+    tmdb.API_KEY = os.environ.get('TMDB_KEY')
+
+    id = tmdb_id
+    if not id:
+        search = tmdb.Search()
+        res = search.movie(query=title, year=year, page=1)
+
+        try:
+            id = res['results'][0]['id']
+        except:
+            return None, None
+
+    movie_result = tmdb.Movies(id)
+    movie = movie_result.info()
+    credits = movie_result.credits()
+    omdb = get_omdb_data(movie['imdb_id'])
+
+    direction = [{'name': i['name'], 'job': i['job'], 'id': i['id']}
+                 for i in credits['crew'] if i['job'] == 'Director']
+    if not direction:
+        direction = omdb['direction']
+
+    writing = [{'name': i['name'], 'job': i['job'], 'id': i['id']}
+               for i in credits['crew'] if i['department'] == 'Writing']
+    if not writing:
+        writing = omdb['writing']
+
+    actors = [{'name': i['name'], 'id': i['id']} for i in credits['cast'][:6]]
+    if not actors:
+        actors = omdb['actors']
+
+    genres = ', '.join([i['name'] for i in movie['genres']])
+    if not genres:
+        genres = omdb['genres']
+
+    imdb_url = f'https://www.imdb.com/find?ref_=nv_sr_sm&q={movie["title"]} {movie["release_date"][:4]}'
+    if movie['imdb_id']:
+        imdb_url = 'https://www.imdb.com/title/' + movie['imdb_id']
+
+    movie_data = {
+        'title': movie['title'],
+        'year': f'({movie["release_date"][:4]})' if movie['release_date'] else '',
+        'overview': movie['overview'],
+        'direction': direction,
+        'writing': writing,
+        'actors': actors,
+        'genres': genres,
+        'poster_path': movie['poster_path'],
+        'imdb-id': movie['imdb_id'],
+        'imdb-rating': omdb['imdb_rating'],
+        'imdb-url': imdb_url,
+        'rotten-tomatoes-rating': omdb['rotten_tomatoes_rating'],
+        'rotten-tomatoes-url': f'https://www.rottentomatoes.com/search?search={movie["title"]}',
+        'metacritic-rating': omdb['metacritic_rating'],
+        'metacritic-url': f'https://www.metacritic.com/search/movie/{movie["title"]}/results',
+        'tmdb-rating': [str(movie['vote_average']) + '/10', float(movie['vote_average'])],
+        'tmdb-url': f'https://www.themoviedb.org/movie/{movie["id"]}'
+    }
+
+    more_results = {'movies': [], 'total_pages': 1}
+
+    if tmdb_id:
+        more_movies = movie_result.similar_movies()
+    else:
+        more_movies = res
+
+    more_results['total_pages'] = more_movies['total_pages']
+    for i in more_movies['results']:
+        try:
+            year = f'({i["release_date"][:4]})' if i['release_date'] else ''
+            more_results['movies'].append(
+                {'title': i['title'], 'year': year, 'tmdb-id': i['id']})
+        except KeyError:
+            more_results['movies'].append(
+                {'title': i['title'], 'year': '', 'tmdb-id': i['id']})
+
+    return movie_data, more_results
+
+
+def get_more_movies(title='', year='', tmdb_id='', page=2):
+    tmdb.API_KEY = os.environ.get('TMDB_KEY')
+
+    id = tmdb_id
+    if not id:
+        search = tmdb.Search()
+        res = search.movie(query=title, year=year, page=page)
+        id = res['results'][0]['id']
+
+    movie_result = tmdb.Movies(id)
+
+    if tmdb_id:
+        # Show similar movies when movie ID es provided
+        more_movies = movie_result.similar_movies(page=page)
+    else:
+        # Show more results from user's query
+        more_movies = res
+
+    results = {
+        'movies': [],
+        'total_pages': more_movies['total_pages']
+    }
+    for i in more_movies['results']:
+        try:
+            year = f'({i["release_date"][:4]})' if i['release_date'] else ''
+            results['movies'].append(
+                {'title': i['title'], 'year': year, 'tmdb-id': i['id']})
+        except KeyError:
+            results['movies'].append(
+                {'title': i['title'], 'year': '', 'tmdb-id': i['id']})
+
+    return results
+
+
+def get_omdb_data(imdb_id):
+    omdb_key = os.environ.get('OMDB_KEY')
+
+    try:
+        url = f'http://www.omdbapi.com/?apikey={omdb_key}&i={imdb_id}'
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    movie = response.json()
+
+    try:
+        direction = movie['Director']
+    except:
+        direction = 'N/A'
+
+    try:
+        writing = movie['Writer']
+    except:
+        writing = 'N/A'
+    
+    try:
+        actors = movie['Actors']
+    except:
+        actors = 'N/A'
+    
+    try:
+        genres = movie['Genre']
+    except:
+        genres = 'N/A'
+
+    try:
+        imdb = movie['Ratings'][0]['Value']
+        imdb = [imdb, float(imdb.split('/')[0])]
+    except (IndexError, ValueError, KeyError):
+        imdb = ['Not available', -1]
+
+    try:
+        rotten_tomatoes = movie['Ratings'][1]['Value']
+        rotten_tomatoes = [rotten_tomatoes, float(rotten_tomatoes[:-1]) / 10]
+    except (IndexError, ValueError, KeyError):
+        rotten_tomatoes = ['Not available', -1]
+
+    try:
+        metacritic = movie['Ratings'][2]['Value']
+        metacritic = [metacritic, float(metacritic.split('/')[0]) / 10]
+    except (IndexError, ValueError, KeyError):
+        metacritic = ['Not available', -1]
+
+    try:
+        poster = movie['Poster']
+    except:
+        poster = ''
+
+    return {
+        'direction': direction,
+        'writing': writing,
+        'actors': actors,
+        'genres': genres,
+        'imdb_rating': imdb,
+        'rotten_tomatoes_rating': rotten_tomatoes,
+        'metacritic_rating': metacritic,
+        'poster_url': poster
+    }
+
+
+def get_letterboxd_rating(imdb_id, title='', year=''):
+
+    try:
+        search_res = requests.get(f'https://letterboxd.com/search/{imdb_id}')
+        search_soup = BeautifulSoup(search_res.text, 'html.parser')
+        search_result = search_soup.ul.li.a['href']
+        movie_url = f'https://letterboxd.com{search_result}'
+        movie = requests.get(
+            f'https://letterboxd.com/csi{search_result}rating-histogram/')
+        movie_soup = BeautifulSoup(movie.text, 'html.parser')
+        movie_rating = movie_soup.find('a', class_='display-rating').text
+
+        return [movie_rating + '/5', float(movie_rating) * 2], movie_url
+    except:
+        url = f'https://letterboxd.com/search/{title} {year}'
+        if movie_soup and '/film/' in search_result:
+            url = movie_url
+
+        return ['Not available', -1], url
+
+
+def get_person(id):
+    tmdb.API_KEY = os.environ.get('TMDB_KEY')
+    person = tmdb.People(id)
+    crew_data = person.movie_credits()['crew']
+    cast_data = person.movie_credits()['cast']
+    jobs = {}
+
+    for movie in crew_data:
+
+        if movie['job'] not in jobs:
+            jobs[movie['job']] = []
+
+        try:
+            year = f'({movie["release_date"][:4]})' if movie['release_date'] else ''
+        except:
+            year = ''
+
+        jobs[movie['job']].append({
+            'title': movie['title'],
+            'year': year,
+            'id': movie['id']
+        })
+    
+    if cast_data:
+        jobs['Actor'] = []
+        for movie in cast_data:
+            try:
+                year = f'({movie["release_date"][:4]})' if movie['release_date'] else ''
+            except:
+                year = ''
+
+            jobs['Actor'].append({
+                'title': movie['title'],
+                'year': year,
+                'id': movie['id']
+            })
+
+    return person.info(), jobs
---
+++ static/movie.js
@@ -0,0 +1,137 @@
+let storage = window.localStorage;
+let current_url = window.location.toString();
+let $loadMore = document.getElementById('load-more');
+let $moreResultsList = document.getElementById('more-results-list');
+let $inputs = document.querySelectorAll('.checkbox div input');
+let $averageRating = document.getElementById('average-rating');
+let title = null;
+let year = null;
+let id = null;
+let page = 2;
+
+if ($loadMore) {
+    title = $loadMore.dataset.title;
+    year = $loadMore.dataset.year;
+    id = $loadMore.dataset.id;
+    $loadMore.addEventListener('click', loadMoreResults);
+}
+
+$inputs.forEach(input => {
+    input.addEventListener('click', updateRating);
+})
+
+updateRating();
+getLetterboxdRating();
+
+
+async function getLetterboxdRating() {
+    let $movie = document.getElementById('movie');
+    let imdbID = $movie.dataset.imdbid;
+    let title = $movie.dataset.title;
+    let year = $movie.dataset.year;
+    let $checkbox = document.getElementsByName('letterboxd-rating')[0];
+    let $label = $checkbox.previousElementSibling;
+    let url = `/letterboxd-rating?id=${imdbID}&t=${title}&y=${year}`;
+    let response;
+
+    if (!storage.getItem(current_url)) {
+        try {
+            response = await fetch(url).then(res => res.json()).then(data => data);
+            storage.setItem(current_url, JSON.stringify(response));
+        } catch (error) {
+            $label.style.color = 'black';
+            $label.innerHTML = 'Letterboxd rating: <span style="color: crimson;">error</span>.';
+            return console.error(error);
+        }
+    } else {
+        response = JSON.parse(storage.getItem(current_url));
+    }
+
+    $label.innerHTML = `<a href="${response.url}" target="_blank">Letterboxd</a> rating: ${response.rating[0]}`;
+    $label.style.color = 'black';
+    $checkbox.dataset.rating = response.rating[1];
+    $checkbox.checked = true;
+    $checkbox.disabled = false;
+    $checkbox.className = 'clickable';
+
+    updateRating();
+
+    if (storage.getItem(current_url)) {
+        try {
+            response = await fetch(url).then(res => res.json()).then(data => data);
+            stored_data = JSON.parse(storage.getItem(current_url));
+
+            if (stored_data.rating[0] != response.rating[0]) {
+                storage.setItem(current_url, JSON.stringify(response));
+                $label.innerHTML = `<a href="${response.url}" target="_blank">Letterboxd</a> rating: ${response.rating[0]}`;
+                $label.style.color = 'black';
+                $checkbox.dataset.rating = response.rating[1];
+                $checkbox.checked = true;
+                $checkbox.disabled = false;
+                $checkbox.className = 'clickable';            
+                updateRating();
+            }
+        } catch (error) {
+            return console.error(error);
+        }
+    }
+}
+
+
+async function loadMoreResults() {
+    let url = `/more-results?t=${title}&y=${year}&id=${id}&p=${page}`;
+    let response;
+
+    try {
+        response = await fetch(url).then(res => res.json()).then(data => data);
+    } catch (error) {
+        return console.error(error);        
+    }
+
+    response.movies.forEach(movie => {
+        let new_movie = document.createElement('li');
+        new_movie.innerHTML = `<a href="/movie/${movie['tmdb-id']}">${movie['title']} ${movie['year']}</a>`;
+        $moreResultsList.appendChild(new_movie);
+    })
+
+    $loadMore.scrollIntoView();
+
+    page++;
+
+    if (page > response['total_pages']) {
+        $loadMore.style.display = 'none';
+    }
+}
+
+
+function updateRating() {
+    let ratingSum = 0;
+    let ratingCount = 0;
+
+    $inputs.forEach(input => {
+        if (input.checked == true && input.dataset.rating != '-1') {
+            ratingSum += Number(input.dataset.rating);
+            ratingCount++;
+        }
+        else if (input.dataset.rating == '-1') {
+            input.disabled = true;
+            input.className = '';
+            input.checked = false;
+
+            let label = input.previousElementSibling;
+            label.style.color = 'grey';
+            if (label.innerText.includes('loading')) {
+                label.style.color = 'black';
+                label.firstElementChild.style.color = '#c58430';
+            }
+        }
+    })
+
+    if (ratingSum == 0) {
+        $averageRating.innerHTML = '-/-';
+        return
+    }
+
+    let average = ratingSum / ratingCount;
+    $averageRating.innerHTML = `${average.toFixed(1)}/10`;
+}
